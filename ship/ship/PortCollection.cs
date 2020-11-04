ae855baf4dc8ff1016a5977bb6f78392bb8729f3
Largik <57515916+Largik@users.noreply.github.com>
2020-11-04 22:11:28 +0400

    ready

---
+++ ship/ship/PortCollection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ship
+{
+    /// <summary>
+    /// Класс-коллекция парковок
+    /// </summary>
+    class PortCollection
+    {
+        /// <summary>
+        /// Словарь (хранилище) с парковками
+        /// </summary>
+        readonly Dictionary<string, Port<Ship, DopDetails>> portStages;
+        /// <summary>
+        /// Возвращение списка названий праковок
+        /// </summary>
+        public List<string> Keys => portStages.Keys.ToList();
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private readonly int pictureWidth;
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private readonly int pictureHeight;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        public PortCollection(int pictureWidth, int pictureHeight)
+        {
+            portStages = new Dictionary<string, Port<Ship, DopDetails>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
+        }
+        /// <summary>
+        /// Добавление парковки
+        /// </summary>
+        /// <param name="name">Название парковки</param>
+        public void AddPort(string name)
+        {
+            if (portStages.ContainsKey(name))
+            {
+                return;
+            }
+            portStages.Add(name, new Port<Ship, DopDetails>(pictureWidth, pictureHeight));
+        }
+        /// <summary>
+        /// Удаление парковки
+        /// </summary>
+        /// <param name="name">Название парковки</param>
+        public void DelPort(string name)
+        {
+            if (portStages.ContainsKey(name))
+            {
+                portStages.Remove(name);
+            }
+        }
+        /// <summary>
+        /// Доступ к парковке
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        public Port<Ship, DopDetails> this[string ind]
+        {
+            get
+            {
+                if (portStages.ContainsKey(ind))
+                {
+                   return portStages[ind];
+                }
+                return null;
+            }
+        }
+    }
+}
