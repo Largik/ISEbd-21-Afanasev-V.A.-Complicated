41d0ca39808d5288a4a8c1be0193829b57c15a1a
Largik <57515916+Largik@users.noreply.github.com>
2020-11-10 10:45:13 +0400

    fix

--- ship/ship/Program.cs
+++ ship/ship/Program.cs
@@ -18,8 +18,6 @@ namespace ship
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPort());
-
-            Console.WriteLine("Hello");
         }
         
     }
